62b9f4b8a28aa56c977e44c20e3aea7e03749607
EC2 Default User <ec2-user@ip-192-168-2-37.ap-northeast-1.compute.internal>
2023-09-12 13:00:36 +0000

    change some names

--- dataSubmission.js
+++ dataSubmission.js
@@ -13,3 +13,17 @@ app.post('/fetchVehicleData', async (req, res) => {
 		res.status(500).json({ error: 'An error occurred while fetching data.' });
 	}
 });
+
+app.post('/fetchGrade', async (req, res) => {
+	try {
+		const dataByGrade = await VehicleAttribute.findAll({
+			attributes: ['grade', [sequelize.fn('COUNT', sequelize.col('grade')), 'count']],
+			group: ['grade'],
+		});
+
+		res.json(dataByGrade);
+	} catch (error) {
+		console.error('Error while fetching data:', error);
+		res.status(500).json({ error: 'An error occurred while fetching data.' });
+	}
+});
