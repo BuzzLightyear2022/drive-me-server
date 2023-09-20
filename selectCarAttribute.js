bb94a986ce498bfc05ec918ad561c292fd1c042c
EC2 Default User <ec2-user@ip-192-168-2-37.ap-northeast-1.compute.internal>
2023-09-20 15:17:48 +0000

    made classes

--- selectCarAttribute.js
+++ selectCarAttribute.js
@@ -3,83 +3,129 @@
 const app = require('./main.js');
 const { sequelize, VehicleAttribute } = require('./tableDefinition');
 
-app.post('/fetchVehicleData', async (req, res) => {
-	try {
-		const allData = await VehicleAttribute.findAll();
-		res.json(allData);
+class selectCarData {
+	static selectRentalClasses = () => {
+		app.post('/selectRentalClasses', async (req, res) => {
+			try {
+				const groupByRentalClass = await VehicleAttribute.findAll({
+					attributes: ['rentalClass'],
+					group: ['rentalClass'],
+				});
 
-	} catch (error) {
-		console.error('Error while fetching data:', error);
-		res.status(500).json({ error: 'An error occurred while fetching data.' });
-	}
-});
-
-app.post('/selectRentalClasses', async (req, res) => {
-	try {
-		const groupByRentalClass = await VehicleAttribute.findAll({
-			attributes: ['rentalClass'],
-			group: ['rentalClass'],
-		});
-
-		res.json(groupByRentalClass);
-	} catch (error) {
-		console.error('Error while fetching data:', error);
-		res.status(500).json({ error: 'An error occurred while fetching data.' });
-	}
-});
-
-app.post('/selectCarModels', async (req, res) => {
-	try {
-		const rentalClass = req.body.rentalClass;
-		const nonSmoking = req.body.nonSmoking;
-		if (nonSmoking === 'none-specification') {
-			const carModels = await VehicleAttribute.findAll({
-				attributes: ["carModel"],
-				where: {
-					rentalClass: rentalClass,
-				},
-				group: ['carModel'],
-			});
-			res.json(carModels);
-		} else if (nonSmoking === 'non-smoking') {
-			const carModels = await VehicleAttribute.findAll({
-				attributes: ["carModel"],
-				where: {
-					nonSmoking: 1,
-					rentalClass: rentalClass
-				},
-				group: ["carModel"]
-			});
-			res.json(carModels);
-		} else if (nonSmoking === 'ok-smoking') {
-			const carModels = await VehicleAttribute.findAll({
-				attributes: ["carModel"],
-				where: {
-					nonSmoking: 0,
-					rentalClass: rentalClass
-				},
-				group: ["carModel"]
-			});
-			res.json(carModels);
-		}
-	} catch (error) {
-		console.error('Error while fetching data:', error);
-		res.status(500).json({ error: 'An error occurred while fetching data.'});
-	}
-});
-
-app.post('/fetchLicensePlate', async (req, res) => {
-	try {
-		const carModel = req.body.carModel;
-		const licensePlate = await VehicleAttribute.findAll({
-			ttributes: ["imageFileName", "licensePlateRegion", "licensePlateCode", "licensePlateHiragana", "licensePlateNumber"],
-			where: {
-				carModel: carModel
+				res.json(groupByRentalClass);
+			} catch (error) {
+				console.error('Error while fetching data:', error);
+				res.status(500).json({ error: 'An error occurred while fetching data.' });
 			}
 		});
-		res.json(licensePlate);
-	} catch (error) {
-		console.error('Error while fetching data:', error);
-		res.status(500).json({ error: 'An error occurred while fetching data.' });
 	}
-});
+
+	static selectCarModels = () => {
+		app.post('/selectCarModels', async (req, res) => {
+			try {
+				const rentalClass = req.body.rentalClass;
+				const nonSmoking = req.body.nonSmoking;
+				if (nonSmoking == 0) {
+					const carModels = await VehicleAttribute.findAll({
+						attributes: ["carModel"],
+						where: {
+							nonSmoking: nonSmoking,
+							rentalClass: rentalClass
+						},
+						group: ["carModel"]
+					});
+					res.json(carModels);
+				} else if (nonSmoking == 1) {
+					const carModels = await VehicleAttribute.findAll({
+						attributes: ["carModel"],
+						where: {
+							nonSmoking: nonSmoking,
+							rentalClass: rentalClass
+						},
+						group: ["carModel"]
+					});
+					res.json(carModels);
+				} else {
+					const carModels = await VehicleAttribute.findAll({
+						attributes: ["carModel"],
+						where: {
+							rentalClass: rentalClass
+						},
+						group: ["carModel"]
+					});
+					res.json(carModels);
+				}
+			} catch (error) {
+				console.error('Error while fetching data:', error);
+				res.status(500).json({ error: 'An error occurred while fetching data.'});
+			}
+		});
+	}
+	static selectLicensePlateAndCarData = () => {
+		app.post('/selectLicensePlateAndCarData', async (req, res) => {
+			try {
+				const rentalClass = req.body.rentalClass;
+				const nonSmoking = req.body.nonSmoking;
+				const carModel = req.body.carModel;
+
+				if (nonSmoking == 0) {
+					const licensePlateAndCarData = await VehicleAttribute.findAll({
+						Attributes: [
+							"id",
+							"imageFileName",
+							"licensePlateRegion",
+							"licensePlateCode",
+							"licensePlateHiragana",
+							"licensePlateNumber"
+						],
+						where: {
+							rentalClass: rentalClass,
+							nonSmoking: nonSmoking
+						}
+					});
+					res.json(licensePlateAndCarData);
+				} else if (nonSmoking == 1) {
+					const licensePlateAndCarData = await VehicleAttribute.findAll({
+						Attributes: [
+							"id",
+							"imageFileName",
+							"licensePlateRegion",
+							"licensePlateCode",
+							"licensePlateHiragana",
+							"licensePlateNumber"
+						],
+						where: {
+							rentalClass: rentalClass,
+							nonSmoking: nonSmoking
+						}
+					});
+					res.json(licensePlateAndCarData);
+
+				} else {
+					const licensePlateAndCarData = await VehicleAttribute.findAll({
+						Attributes: [
+							"id",
+							"imageFileName",
+							"licensePlateRegion",
+							"licensePlateCode",
+							"licensePlateHiragana",
+							"licensePlateNumber"
+						],
+						where: {
+							carModel: carModel,
+							rentalClass: rentalClass,
+						}
+					});
+					res.json(licensePlateAndCarData);
+				}
+			} catch (error) {
+				console.error('Error while fetching data:', error);
+				res.status(500).json({ error: 'An error occurred while fetching data.' });
+			}
+		});
+	}
+}
+
+selectCarData.selectRentalClasses();
+selectCarData.selectCarModels();
+selectCarData.selectLicensePlateAndCarData();
