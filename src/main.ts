958060e6b560626d48a9bde35908d91d71a5be5d
Takehiro Mizuno <coder-mizuno@fg8.so-net.ne.jp>
2024-02-28 14:33:00 +0900

    HTPPS

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,6 @@ app.use(express.json());
 app.use(cors());
 app.use("/C2cFbaAZ", express.static("./car_images"));
 
-const httpsServer = https.createServer(app);
 const server = http.createServer(app);
 
 const WsServer: WebSocket.Server<typeof WebSocket, typeof http.IncomingMessage> = new WebSocket.Server({ server });
@@ -579,8 +578,25 @@ app.post("/sqlUpdate/reservationData", upload.fields([
 	}
 })();
 
+const letsencryptDirectory = path.join("etc", "letsencrypt", "live", "drive-me-test.com");
+const privateKeyPath = path.join(letsencryptDirectory, "privkey.pem");
+const certificatePath = path.join(letsencryptDirectory, "cert.pem");
+const chainFilePath = path.join(letsencryptDirectory, "chain.pem");
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const certificate = fs.readFileSync(certificatePath, "utf8");
+const chainFile = fs.readFileSync(chainFilePath, "utf8");
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: chainFile
+}
+
+const httpsServer = https.createServer(credentials, app);
+
 httpsServer.listen(httpsPort, () => {
-	console.log(`Server start on port: ${httpsPort}`);
+	console.log(`HTTPS Server running on port: ${httpsPort}`);
 });
 
 server.listen(port, () => {
